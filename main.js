dd69543ef86822c5c52d8e49fde310dae69ce93c
Vihaan875 <76944903+Vihaan875@users.noreply.github.com>
2021-08-17 16:41:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preLoad()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('myFilter_image.png');
+}
